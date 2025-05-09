754cf67c66fa71a6b3e61b13fd673a8ef9d43aed
luis123456789-art <garcesluisalfredo56@gmail.com>
2025-05-09 16:21:34 -0500

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,6 +7,5 @@ export default{
     host : process.env.HOST,
     database : process.env.DATABASE,
     user: process.env.USER,
-    password: process.env.PASSWORD,
-    port: process.env.PORT
+    password: process.env.PASSWORD
 }
